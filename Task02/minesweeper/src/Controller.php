3de5d262d0704b663a55480784dc3effa2d8362d
Andrey Popov <andrvpopov@gmail.com>
2021-01-25 16:14:45 +0300

    Delete tasks from DB_Applications course

--- Task02/minesweeper/src/Controller.php
+++
@@ -1,8 +0,0 @@
-<?php namespace plaksindv\minesweeper\Controller;
-    use function plaksindv\minesweeper\View\showGame;
-    
-    function startGame() {
-        echo "Game started".PHP_EOL;
-        showGame();
-    }
-?>
--- Task02/minesweeper/src/View.php
+++
@@ -1,5 +0,0 @@
-<?php namespace plaksindv\minesweeper\View;
-    function showGame() {
-        echo "This is game intereface";
-    }
-?>
